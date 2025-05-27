1f3c2ba04dd79206ec60c35336a7d4427f465dd2
VaishnavMeet <162859687+VaishnavMeet@users.noreply.github.com>
2025-05-27 12:01:40 +0530

    Done with Animation
    Hurt
    Attack
    Run

--- Assets/Script/EnemyFollow.cs
+++ Assets/Script/EnemyFollow.cs
@@ -3,14 +3,20 @@ using Pathfinding;
 using System.Collections;
 
 [RequireComponent(typeof(AIDestinationSetter))]
+[RequireComponent(typeof(AILerp))]
+[RequireComponent(typeof(Animator))]
 public class EnemyFollow : MonoBehaviour
 {
     private AIDestinationSetter destinationSetter;
+    private AILerp aiLerp;
+    private Animator animator;
     private Coroutine setTargetCoroutine;
-
+    public Animator playerAnimator;
     void Awake()
     {
         destinationSetter = GetComponent<AIDestinationSetter>();
+        aiLerp = GetComponent<AILerp>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -18,8 +24,7 @@ public class EnemyFollow : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("Trigger entered, starting coroutine");
-            
-            // If a coroutine is already running, stop it first
+            playerAnimator=other.gameObject.GetComponent<Animator>();
             if (setTargetCoroutine != null)
                 StopCoroutine(setTargetCoroutine);
 
@@ -27,12 +32,49 @@ public class EnemyFollow : MonoBehaviour
         }
     }
 
-  
-
     private IEnumerator SetTargetAfterDelay(Transform target, float delay)
     {
         yield return new WaitForSeconds(delay);
+        animator.SetBool("IsMoving", true);
         destinationSetter.target = target;
-        Debug.Log("Target set after delay");
+    }
+
+    void Update()
+    {
+       
+
+        if (destinationSetter.target != null)
+        {
+            float distanceToTarget = Vector2.Distance(transform.position, destinationSetter.target.position);
+
+            if (distanceToTarget <= 0.3f)
+            {
+                aiLerp.canMove = false; // Stop moving
+                RotateTowardsMovement(0);
+                animator.SetBool("IsAttack", true);
+                animator.SetBool("IsMoving", false);
+                playerAnimator.SetBool("IsHurted", true);
+            }
+            else
+            {
+                aiLerp.canMove = true; // Resume moving
+                RotateTowardsMovement(90);
+                animator.SetBool("IsAttack", false);
+                animator.SetBool("IsMoving", true);
+                playerAnimator.SetBool("IsHurted", false);
+            }
+        }
+    }
+
+
+    private void RotateTowardsMovement(float offset=90)
+    {
+        Vector2 velocity = aiLerp.velocity;
+
+        if (velocity.sqrMagnitude > 0.01f)
+        {
+            float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, angle - offset);
+        }
     }
 }
